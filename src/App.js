f30281b0ccd3276f7230ffd8dd6cd2340b4b5785
Rachael Metcalf <raespark.games@gmail.com>
2018-06-14 01:15:36 -0500

    Add reactstrap, add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import Navbar from './components/Navbar/Navbar'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.links = [
+      {
+        url: '#AboutMe',
+        text: 'About Me'
+      },
+      {
+        url: '#Projects',
+        text: 'Projects'
+      },
+      {
+        url: '#Experience',
+        text: 'Experience'
+      },
+      {
+        url: '#Contact',
+        text: 'Contact Me'
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
+        <Helmet title="Rachael Metcalf"/>
+        <Navbar links={this.links}/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+import './Navbar.css';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Rachael Metcalf</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+                {this.props.links.map((link) => (
+                    <NavItem>
+                        <NavLink href={link.url}>{link.text}</NavLink>
+                    </NavItem>
+                ))}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
