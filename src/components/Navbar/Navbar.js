5800e6e2ec30546d9623de54e72531f44e10d667
Rachael Metcalf <raespark.games@gmail.com>
2018-06-14 18:16:49 -0500

    switch to pure bootstrap instead of reactstrap to easier customize styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,48 +1,25 @@
 import React from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
 import './Navbar.css';
 
 export default class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Rachael Metcalf</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-                {this.props.links.map((link) => (
-                    <NavItem>
-                        <NavLink href={link.url}>{link.text}</NavLink>
-                    </NavItem>
-                ))}
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="nav">
+                <nav className="navbar navbar-expand-sm navbar-light bg-light navbar-fixed">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            {this.props.links.map((link) => (   
+                                <li className="nav-item">
+                                    <a className="nav-link" href={link.url}>{link.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
 }
