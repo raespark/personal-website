0ce22a69fd0dbf1ea3ad01f24d83742913c94ce5
Rachael Metcalf <raespark.games@gmail.com>
2018-06-15 00:45:52 -0500

    Replace placeholder project section with single project

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SectionTitle from '../SectionTitle/SectionTitle';
+import majopon from '../../Assets/majopon.gif';
 import './Projects.css';
 
 class Projects extends Component {
@@ -8,17 +9,24 @@ class Projects extends Component {
     return (
         <div>
         <SectionTitle title="Projects"/>
-        <p className="projects-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis convallis tellus id interdum velit laoreet id donec. Id nibh tortor id aliquet lectus proin nibh nisl condimentum. Semper eget duis at tellus at. Enim ut tellus elementum sagittis vitae et leo duis. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod. Non pulvinar neque laoreet suspendisse interdum consectetur. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan. Magna fringilla urna porttitor rhoncus dolor purus non enim. Eu mi bibendum neque egestas. Bibendum enim facilisis gravida neque convallis a cras semper auctor. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Suspendisse in est ante in nibh mauris cursus mattis. Nunc non blandit massa enim. Adipiscing diam donec adipiscing tristique risus nec feugiat in.
-
-Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Massa vitae tortor condimentum lacinia quis vel eros. In aliquam sem fringilla ut morbi tincidunt. Id ornare arcu odio ut sem. Gravida in fermentum et sollicitudin ac orci phasellus egestas tellus. Quam vulputate dignissim suspendisse in est ante in nibh mauris. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Donec adipiscing tristique risus nec feugiat. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Semper feugiat nibh sed pulvinar proin. Ut consequat semper viverra nam. In cursus turpis massa tincidunt dui ut ornare lectus. Quis auctor elit sed vulputate mi sit amet. Hendrerit gravida rutrum quisque non tellus orci ac. Feugiat nibh sed pulvinar proin gravida. Elementum integer enim neque volutpat ac tincidunt. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Non blandit massa enim nec dui nunc mattis.
-
-Interdum velit laoreet id donec ultrices tincidunt. Dui faucibus in ornare quam viverra. Eros in cursus turpis massa tincidunt. Sit amet consectetur adipiscing elit ut aliquam purus sit amet. Aliquam sem fringilla ut morbi tincidunt augue. Vel turpis nunc eget lorem. Orci dapibus ultrices in iaculis nunc. Posuere sollicitudin aliquam ultrices sagittis orci. Ullamcorper sit amet risus nullam eget. Et pharetra pharetra massa massa ultricies. Vitae tempus quam pellentesque nec. Tellus at urna condimentum mattis pellentesque id.
-
-Volutpat blandit aliquam etiam erat velit. A scelerisque purus semper eget duis at tellus. Integer feugiat scelerisque varius morbi enim nunc faucibus a. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Suspendisse in est ante in nibh. Imperdiet dui accumsan sit amet nulla. In hac habitasse platea dictumst quisque sagittis. Quam viverra orci sagittis eu volutpat odio facilisis. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Integer eget aliquet nibh praesent tristique magna sit. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Semper viverra nam libero justo laoreet sit. Amet dictum sit amet justo donec enim diam vulputate.
-
-Porta non pulvinar neque laoreet suspendisse. Donec massa sapien faucibus et molestie ac. Sed vulputate mi sit amet. Mattis enim ut tellus elementum. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Ut sem viverra aliquet eget. Sodales ut etiam sit amet. Dis parturient montes nascetur ridiculus mus. Adipiscing diam donec adipiscing tristique risus nec feugiat in. Nisl purus in mollis nunc sed id semper risus in. Facilisis magna etiam tempor orci eu lobortis. Tristique magna sit amet purus gravida quis blandit turpis. Amet consectetur adipiscing elit duis tristique sollicitudin.
-        </p>
+        <div className="row">
+          <div className="col-sm-12 order-md-last col-md-6 project-image">
+            <img className="projects-image" src={majopon}/>
+          </div>
+          <div className="col-sm-12 order-md-first col-md-6 projects-text">
+            <h4 className="project-title">Majopon (Pictured)</h4>
+            <p className="project-description">
+            A pong-like game made for the <a href="https://itch.io/jam/makecutepong">Femicom make cute pong game jam.</a> 
+            Made in the <a href="https://godotengine.org/">Godot engine</a> over the course of about 2 weeks, 
+            as a completely independent project.
+            </p>
+            <h5>links:</h5>
+            <div className="link-list">
+              <li><a href="">Download</a></li>
+              <li><a href="">Source</a></li>
+            </div>
+          </div>
+        </div>
         </div>
     );
   }
