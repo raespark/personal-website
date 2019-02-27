e18a74de231738296766833b7af4652d21c549c9
Rachael Metcalf <raespark.games@gmail.com>
2019-02-26 23:19:11 -0600

    fix more typos

--- src/Components/About/WhoAmI/WhoAmI.js
+++ src/Components/About/WhoAmI/WhoAmI.js
@@ -5,9 +5,9 @@ import ParagraphImage from '../../ParagraphImage/ParagraphImage';
 class WhoAmI extends Component {
   render() {
     let workAbout = 'Hi, I\'m Rachael!  I\'ve been a professional web developer since June of 2017, ' +
-    'and graduated with a Bachelor of Science in Computer Science from UT Austin. I\'m a fullstack engineer with a focus in UI,  ' +
+    'and graduated with a Bachelor of Science in Computer Science from UT Austin. I\'m a full stack engineer with a focus in UI,  ' +
     'and have a passion for good UX.  I also have experience leading multiple projects, organizations, ' + 
-    'working with people of all technical backgrounds, and designing applications, and taking them from concept to release.  '+
+    'working with people of all technical backgrounds, and designing applications, taking them from concept to release.  '+
     'I\'m always excited and amazed with every new thing I learn with each project I work on, and can\'t wait to see what my future holds!';
 
     let hobbiesAbout = 'Outside of work, I have lots of things I like to do! I enjoy drawing, gaming, and even play music sometimes.  ' +
