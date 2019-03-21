09717a82f41eac1c0fd56fc2a68269a811d20881
Rachael Metcalf <raespark.games@gmail.com>
2019-03-21 16:39:52 -0500

    fix typos in Accomplishments section

--- src/Components/About/Accomplishments/Accomplishments.js
+++ src/Components/About/Accomplishments/Accomplishments.js
@@ -5,9 +5,9 @@ class Accomplishments extends Component {
   render() {
     let accomplishments = [
       {text: 'Assisted in training/mentoring new hires and interns starting at the company. ' + 
-      'Helping them to learn new skills, learn new tools and boosting their comfidence.'},
-      {text: 'Took on a large, long-term project and broke it down into smaller, managable pieces. ' + 
-      'Handled the planning, prioritzation, and management for the project through to completion while also working on the code.'},
+      'Helping them to learn new skills, learn new tools and boosting their confidence.'},
+      {text: 'Took on a large, long-term project and broke it down into smaller, managaTook on large, long-term projects and broke them down into smaller, manageable pieces. ' + 
+      'Handled the planning, prioritization, and management for the project through to completion while also working on the code.'},
       {text: 'Given multiple presentations on both technical and non technical subjects in front of various audiences including C-level executives.'}
     ]
     return (
