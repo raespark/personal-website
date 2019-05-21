127cdcc86c482230398ba8671e9802fbd949664d
Rachael Metcalf <raespark.games@gmail.com>
2019-05-21 10:14:59 -0500

    fix typo/merge issue with accomplishments text

--- src/Components/About/Accomplishments/Accomplishments.js
+++ src/Components/About/Accomplishments/Accomplishments.js
@@ -6,7 +6,7 @@ class Accomplishments extends Component {
     let accomplishments = [
       {text: 'Assisted in training/mentoring new hires and interns starting at the company. ' + 
       'Helping them to learn new skills, learn new tools and boosting their confidence.'},
-      {text: 'Took on a large, long-term project and broke it down into smaller, managaTook on large, long-term projects and broke them down into smaller, manageable pieces. ' + 
+      {text: 'Took on large, long-term projects and broke them down into smaller, manageable pieces. ' + 
       'Handled the planning, prioritization, and management for the project through to completion while also working on the code.'},
       {text: 'Given multiple presentations on both technical and non technical subjects in front of various audiences including C-level executives.'}
     ]
